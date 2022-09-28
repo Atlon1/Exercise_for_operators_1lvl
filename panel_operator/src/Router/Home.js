325e351512e80f3df2fdafa4409669eb1340694f
Maciej Szajstek <plsm44@europe.phoenixcontact.com>
2022-09-28 18:16:09 +0200

    Structure is ready

---
+++ panel_operator/src/Router/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import '../scss/main.scss';
+import Navigation from "../components/Navigation";
+
+
+
+
+export default function Home() {
+    return (
+        <>
+            <Navigation/>
+        </>
+    )
+}
---
+++ panel_operator/src/components/Navigation.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+
+const Navigation = () => {
+
+
+    return(
+        <section className='navigation'>
+        <div>Tu bedzie nawigacja</div>
+        </section>
+    )
+}
+
+export default Navigation;
